40b40f6f6587ae1740b26d526c0b61e3b1556edf
marleybaker11 <127899059+marleybaker11@users.noreply.github.com>
2023-03-28 21:19:34 +0000

    Marley

--- PiDay.java
+++ PiDay.java
@@ -2,5 +2,22 @@ public class PiDay {
     public static void main(String[] args) {
         System.out.println();
     }
+    public static double piday(int n, boolean subract){
+        if(n >= 1000){
+            return 0;
+        }else{
+            if(subract == true){
+                return -4.0/n + piday(n+2, false);
 
+            }else{
+                return 4.0/n + piday(n+2, true);
+
+            }
+            
+        }
+    public static void main(String[] args) {
+        double pi = piday(1, false);  
+        System.out.println("pi +" + pi);
+    
+    }
 }
