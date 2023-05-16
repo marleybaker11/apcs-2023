9ac961925174da74afff8ade7fde6b39ac069278
marleybaker11 <127899059+marleybaker11@users.noreply.github.com>
2023-05-16 21:43:35 +0000

    Marley and Max

--- Stats.java
+++ Stats.java
@@ -5,6 +5,7 @@ Activity 4
  */
  import core.data.*;
  import java.util.ArrayList;
+ import java.util.Scanner;
 public class Stats{
       
       private String name;
@@ -12,15 +13,19 @@ public class Stats{
       private int year;
       private int attempts;
       private double percent;
+      private static int date;
       
    public static void main (String args[]){
       DataSource ds = DataSource.connect("Career_Stats_Passing.csv");
       ds.load();
       ArrayList<Stats> allQuarterbacks = ds.fetchList("Stats", "Name", "Team", "Year", "Passes Attempted", "Completion Percentage");
-      System.out.println(allQuarterbacks.size());
-      Stats bestGuy = getBest(allQuarterbacks);
+      Scanner keyboard = new Scanner(System.in);
+      System.out.println("Enter year: ");
+      int date = keyboard.nextInt();
+      Stats bestGuy = getBest(allQuarterbacks, date);
       System.out.println(bestGuy);
-     }   
+   }   
+
       public Stats(String n, String t, int y, int a, double p){
             this.name = n;
             this.team = t;
@@ -28,23 +33,19 @@ public class Stats{
             this.attempts = a;
             this.percent = p;               
       }
-       public static Stats getBest(ArrayList<Stats> allQuarterbacks){
-            for(int col = 0; col < allQuarterbacks.size(); col ++){
-            if(allQuarterbacks.get(col).percent == 100) {
-                 return allQuarterbacks.get(col);
-           
-            //if (attempts > 100){
-                //return allQuarterbacks;
-              
+       
+      public static Stats getBest(ArrayList<Stats> allQuarterbacks, int date){
+          int temp = 0;  
+          for(int col = 0; col < allQuarterbacks.size(); col ++){
+            if(allQuarterbacks.get(col).attempts >= 100 && allQuarterbacks.get(temp).percent < allQuarterbacks.get(col).percent && allQuarterbacks.get(col).year == date) {
+               temp = col;               
             }
          }
-         return null;
+         return allQuarterbacks.get(temp);
        }        
-              
-      
-      
+                 
       public String toString(){
-              return "The Quarterback with the best completion rate is " + name + "in " + year + " with a completion percent of " + percent + "%.";
+              return "The Quarterback with the best completion percentage in " + year + " was " + name + " with a completion percent of " + percent + "% on the " + team + ".";
       }
       
       
