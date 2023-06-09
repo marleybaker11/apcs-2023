aa889865a474634bc0d3a149429e0a58395d43f6
marleybaker11 <127899059+marleybaker11@users.noreply.github.com>
2023-06-09 21:34:10 +0000

    Bee

--- unit10/lab02bee/SpellingBee.java
+++ unit10/lab02bee/SpellingBee.java
@@ -3,18 +3,42 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class SpellingBee {
 
     private char[] letters;
     private char mustUse;
 
-    // TODO construct me!
-
-    public boolean checkWord(String word) {
-        // TODO implement me!
-        return true;
+    public SpellingBee(char[] letters, char mustUse) {
+        this.letters = letters;
+        this.mustUse = mustUse;
     }
+
+ public boolean checkWord(String word) {
+    // Check if the word contains only the required letters and the must-use letter
+    // Must be at least 4 letters long
+    if (word.length() < 4) {
+        return false;
+    }
+    
+    for (char c : word.toCharArray()) {
+        if (!containsLetter(c)) {
+            return false;
+        }
+    }
+    
+    return word.indexOf(mustUse) != -1;
+}
+
+private boolean containsLetter(char letter) {
+    for (char c : letters) {
+        if (c == letter) {
+            return true;
+        }
+    }
+    return false;
+}
 
     /**
      * Loads the contents of file "filename" as a String.
@@ -28,9 +52,7 @@ public class SpellingBee {
         try {
             path = Path.of(ClassLoader.getSystemResource(filename).toURI());
             contents = Files.readString(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
         return contents;
@@ -39,44 +61,23 @@ public class SpellingBee {
     public static void main(String[] args) {
         String[] words = loadFile("words_dropped.txt").split("\n");
         System.out.println("Loaded " + words.length + " words");
-        // TODO solve me!
-        // SpellingBee bee = new SpellingBee("ranglty".toCharArray(), 'y');
 
-        // TODO sort words!
-
-        // TODO what position in the sorted list is the word "search" ?
-         Arrays.sort(words);
-        int n = 0;
-        for(String word:words) {
-            n++;
-            if(word.equals("potato")) {
-                System.out.println("found potato at " +n) ;
+        SpellingBee bee = new SpellingBee("rmnuiaj".toCharArray(), 'n');
+        for(String word:words){
+            if(bee.checkWord(word)){
+                System.out.println(word);
             }
         }
-        String findMe = "search";
-        int begin = 0;
-        int end = words.length-1;
-        int guess = (begin+end)/2;
-        int check = words[guess].compareTo[findMe];
-        System.out.println("word is "=words[guess]);
-        System.out.println("check is " +check);
 
-        begin = guess;
-        guess = (begin+end)/2;
-        check = words[guess].compareTo[findMe];
-        System.out.println("word is "=words[guess]);
-        System.out.println("check is " +check);
-        loop {
-            if(check < 0) {
-                begin = guess;
-            }else if(check > 0){
-                end = guess;
-            }
-            guess = (begin+end)/2;
-            check = words[guess].compareTo[findMe];
-        System.out.println("word is "=words[guess]);
-        System.out.println("check is " +check);
-         }
-       }
+        // Sort the words array
+        Arrays.sort(words);
+
+        // Find the position of the word "search" in the sorted list
+        int index = Arrays.binarySearch(words, "search");
+        if (index >= 0) {
+            System.out.println("Found 'search' at position " + (index + 1));
+        } else {
+            System.out.println("Word 'search' not found.");
+        }
     }
 }
